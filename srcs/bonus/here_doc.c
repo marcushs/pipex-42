9810b0f751f59b31a0b94f78b0da002065987bd5
Hok sum Leung <hleung@z2r8p4.42lyon.fr>
2023-04-18 15:13:38 +0200

    added start_heredoc, need gnl

--- srcs/bonus/here_doc.c
+++ srcs/bonus/here_doc.c
@@ -6,7 +6,32 @@
 /*   By: hleung <hleung@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 14:42:59 by hleung            #+#    #+#             */
-/*   Updated: 2023/04/18 14:43:01 by hleung           ###   ########lyon.fr   */
+/*   Updated: 2023/04/18 15:11:15 by hleung           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	*get_fd(void)
+{
+	int	*fd;
+
+	fd = (int *)malloc(sizeof(int) * 2);
+	if (!fd)
+	{
+		perror("Malloc error");
+		return (NULL);
+	}
+	if (pipe(fd) == -1)
+	{
+		perror("Error opening pipe");
+		return (NULL);
+	}
+	return (fd);
+}
+
+void	start_heredoc(void)
+{
+	int	*fd;
+
+	fd = get_fd();
+	
+}
