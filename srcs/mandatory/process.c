2c611036b57206aeab57324bda295bdbfa6fb5dc
Marcus Hok Sum Leung <marcus.hoksum.leung@gmail.com>
2023-04-24 15:55:24 +0200

    still pb with close

--- srcs/mandatory/process.c
+++ srcs/mandatory/process.c
@@ -12,7 +12,7 @@
 
 #include "../../includes/pipex.h"
 
-// static void	close_pipes(int	*fd)
+// static void	close_pipes(int	fd[2])
 // {
 // 	close(fd[0]);
 // 	close(fd[1]);
@@ -39,18 +39,14 @@
 static void	first_child(t_pipex *pipex, int fd[2], char **envp)
 {
 	close(fd[0]);
-	close(pipex->outfile);
+	if (pipex->outfile != -1)
+		close(pipex->outfile);
 	if (dup2(pipex->infile, STDIN_FILENO) == -1)
-	{
-		free_pipex(pipex);
-		strerror_exit();
-	}
-	close(pipex->infile);
+		free_pipex_exit(pipex);
+	if (pipex->infile != -1)
+		close(pipex->infile);
 	if (dup2(fd[1], STDOUT_FILENO) == -1)
-	{
-		free_pipex(pipex);
-		strerror_exit();
-	}
+		free_pipex_exit(pipex);
 	close(fd[1]);
 	if (!pipex->cmd1)
 		exit(EXIT_FAILURE);
@@ -60,19 +56,15 @@ static void	first_child(t_pipex *pipex, int fd[2], char **envp)
 static void	second_child(t_pipex *pipex, int fd[2], char **envp)
 {
 	close(fd[1]);
-	close(pipex->infile);
+	if (pipex->infile != -1)
+		close(pipex->infile);
 	if (dup2(fd[0], STDIN_FILENO) == -1)
-	{
-		free_pipex(pipex);
-		strerror_exit();
-	}
+		free_pipex_exit(pipex);
 	close(fd[0]);
 	if (dup2(pipex->outfile, STDOUT_FILENO) == -1)
-	{
-		free_pipex(pipex);
-		strerror_exit();
-	}
-	close(pipex->outfile);
+		free_pipex_exit(pipex);
+	if (pipex->outfile != -1)
+		close(pipex->outfile);
 	if (!pipex->cmd2)
 		exit(EXIT_FAILURE);
 	execve(pipex->cmd2, pipex->cmd2_strs, envp);
@@ -81,30 +73,26 @@ static void	second_child(t_pipex *pipex, int fd[2], char **envp)
 void	launch_processes(t_pipex *pipex, char **envp)
 {
 	int	fd[2];
-	
-	pipe(fd);
-	// if (!pipex->fd)
-	// 	free_pipex(pipex);
+
+	if (pipe(fd) == -1)
+		free_pipex_exit(pipex);
 	pipex->pid1 = fork();
 	if (pipex->pid1 == -1)
-	{
-		perror("Error forking process");
-		free_pipex(pipex);
-	}
+		free_pipex_exit(pipex);
 	if (pipex->pid1 == 0)
 		first_child(pipex, fd, envp);
 	pipex->pid2 = fork();
 	if (pipex->pid2 == -1)
-	{
-		perror("Error forking process");
-		free_pipex(pipex);
-	}
+		free_pipex_exit(pipex);
 	if (pipex->pid2 == 0)
 		second_child(pipex, fd, envp);
+	// close_pipes(fd);
 	close(fd[0]);
 	close(fd[1]);
-	close(pipex->infile);
-	close(pipex->outfile);
+	if (pipex->infile != -1)
+		close(pipex->infile);
+	if (pipex->outfile != -1)
+		close(pipex->outfile);
 	wait_status(pipex, 1);
 	wait_status(pipex, 2);
 }
